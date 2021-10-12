86831296a0013ebe1c1bf79359d7c102451a8f96
mso872 <mso872@gmail.com>
2021-10-12 14:36:32 -0700

    Added js-sha256 to library and added requirement

---
+++ blockchain.js
@@ -0,0 +1,22 @@
+let sha256 = require('./js-sha256')
+
+class Blockchain {
+    constructor(genesisBlock) {
+        this.blocks = [];
+        this.addBlock(genesisBlock)
+    }
+
+    addBlock(block) {
+        if (this.blocks.length == 0) {
+            block.previousHash = "00000000000000000"
+            block.hash = this.generateHash(block)
+        }
+
+        this.blocks.push(block)
+    }
+
+    generateHash(block) {
+
+    }
+
+}
