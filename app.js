c4ca48f6c74e4b1c2163845fc6738cf9e8f855f3
mso872 <mso872@gmail.com>
2021-10-12 21:52:58 -0700

    Added Block module; created block object

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ let transaction = new Transaction('Mary','Jerry',100)
 let genesisBlock = new Block()
 let blockchain = new Blockchain(genesisBlock)
 
+let block = blockchain.getNextBlock([transaction])
+blockchain.addBlock(block)
+
 console.log(blockchain)
